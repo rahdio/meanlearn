81eaa8de39df51ea7520d65c2f8ad8342337dbd6
unknown <oduaht@gmail.com>
2015-09-11 11:37:36 +0100

    modified routes

--- app/controllers/list_controller.js
+++ app/controllers/list_controller.js
@@ -1,3 +1,18 @@
 module.exports.output = function(req, res){
-	res.send("postIT - view list of posts")
+	
+	switch(req.method){
+		case "GET":
+			res.send("postIT - listing of available posts with id - "+req.params.id)
+			break;
+		case "POST":
+			res.send("postIT - adding a new post to collection")
+			break;
+		case "PUT":
+			res.send("postIT - replacing collection by value")
+			break;
+		case "DELETE":
+			res.send("postIT - deleting entry from list")
+			break;
+	}
+
 }
--- app/routes/list_route.js
+++ app/routes/list_route.js
@@ -2,5 +2,8 @@ module.exports = function(app){
 	var logController = require('../controllers/log_controller')
 	var listController = require('../controllers/list_controller')
 	
-	app.get('/posts', logController, listController.output)
+	app.get('/posts/:id', logController, listController.output)
+	app.post('/posts/:id', logController, listController.output)
+	app.put('/posts/:id', logController, listController.output)
+	app.delete('/posts/:id', logController, listController.output)
 }
